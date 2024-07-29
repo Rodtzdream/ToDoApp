8c4f95ed5f98f41099a567a8da32e66e4323ec4a
Yurii Oliinyk <olijnikura@gmail.com>
2024-07-29 11:48:22 +0300

    Feature/api project (#2)
    * Added API Project
    
    * Registered DbContext in the Api project

---
+++ ToDoApp.Api/Controllers/ToDoItemsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoApp.Data.Context;
+using ToDoApp.Data.Models;
+
+namespace ToDoApp.Api.Controllers
+{
+    [Route("api/to-do-items")]
+    [ApiController]
+    public class ToDoItemsController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+
+        public ToDoItemsController(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDoItem>>> GetAsynk()
+        {
+            var items = await _context.ToDoItems.ToListAsync();
+            return Ok(items);
+        }
+    }
+}

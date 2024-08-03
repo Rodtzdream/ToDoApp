bb4e56649a2cb67390810f1578095c1b53ec6577
Yurii Oliinyk <olijnikura@gmail.com>
2024-08-03 17:59:05 +0300

    Fixed: Replace StatusEnum with int for id parameters

--- ToDoApp.Services/Exceptions/ToDoItemNotFoundException.cs
+++ ToDoApp.Services/Exceptions/ToDoItemNotFoundException.cs
@@ -5,7 +5,7 @@ namespace ToDoApp.Services.Exceptions;
 
 public class ToDoItemNotFoundException : ApplicationBaseException
 {
-    public ToDoItemNotFoundException(StatusEnum id) : base($"ToDo item with id {id} was not found", HttpStatusCode.NotFound)
+    public ToDoItemNotFoundException(int id) : base($"ToDo item with id {id} was not found", HttpStatusCode.NotFound)
     {
     }
 }
